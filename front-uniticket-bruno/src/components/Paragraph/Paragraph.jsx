6b1add3cc4e3ae9dd93bc2985ede388cdab4ba4d
bruno.suarez <contatobrunosuarez@gmail.com>
2023-02-13 19:02:15 -0300

    clean

--- front-uniticket-bruno/src/components/Paragraph/Paragraph.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function Paragraph({className, text}) {
-  return (
-    <p className={className}> {text} </p>
-  );
-}
