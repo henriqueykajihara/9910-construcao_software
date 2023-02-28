bfef5f0ff503fc4d3625fa0dc3533b246900973a
bruno.suarez <contatobrunosuarez@gmail.com>
2023-02-28 16:38:35 -0300

    adjustament project front

--- src/app/Routes/Example/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-// import { Container } from './styles';
-
-export default class Example extends Component {
-  render() {
-    return (<h1>Exemplo</h1>);
-  }
-}
--- src/app/Routes/Historico/index.jsx
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import Button from '../../../components/Button/Button';
-
-export default class Historico extends Component {
-}
